794dad5e874cea23a4e8da71713097ad206c29db
brettl1991 <brettl1991@gmail.com>
2023-02-08 15:57:14 +0000

    feat: split ExpenseItem into new component ExpenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,12 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const month = props.date.toLocaleString("en-UK", { month: "long" });
-  const day = props.date.toLocaleString("en-UK", { day: "2-digit" });
-  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
