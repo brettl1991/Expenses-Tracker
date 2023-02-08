89e4c04855385612e694505d052f98bdb3e70c2b
brettl1991 <brettl1991@gmail.com>
2023-02-08 15:23:56 +0000

    feat: add js logic to date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,9 +3,16 @@ import React from "react";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const month = props.date.toLocaleString("en-UK", { month: "long" });
+  const day = props.date.toLocaleString("en-UK", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
