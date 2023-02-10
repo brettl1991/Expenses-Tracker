02cdda5611be8a495fa0327b6bec9c657fd30b55
brettl1991 <brettl1991@gmail.com>
2023-02-10 11:58:14 +0000

    feat: create card and expenses components and compose them to avoid double written code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
+import React from "react";
+import Expenses from "./components/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -29,29 +30,10 @@ function App() {
   ];
   return (
     <div>
-      <h1>Hello</h1>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import React from "react";
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 
 import "./ExpenseItem.css";
 
-const ExpenseItem = (props) => {
+const ExpenseItem = ({ date, title, amount }) => {
   return (
-    <div className="expense-item">
-      <ExpenseDate date={props.date} />
+    <Card className="expense-item">
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">£{props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">£{amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
